952a1c566b55c1e1d8e66111c09d198a7f06ae4b
Parth Arya <partharya@1x-nat-vl932-172-30-243-1.wireless.umass.edu>
2017-12-08 10:16:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/partharya/scanner/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.partharya.scanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
+    private ZXingScannerView zXingScannerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void scan(View view){
+        zXingScannerView =new ZXingScannerView(getApplicationContext());
+        setContentView(zXingScannerView);
+        zXingScannerView.setResultHandler(this);
+        zXingScannerView.startCamera();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        zXingScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+
+//        Intent intent = new Intent(MainActivity.this, SavedProfiles.class);
+//        startActivity(intent);
+        Toast.makeText(getApplicationContext(),result.getText(),Toast.LENGTH_SHORT).show();
+        zXingScannerView.resumeCameraPreview(this);
+
+    }
+}
---
+++ app/src/main/java/com/example/partharya/scanner/SavedProfiles.java
@@ -0,0 +1,21 @@
+package com.example.partharya.scanner;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class SavedProfiles extends AppCompatActivity{
+
+    public String[] infoArray;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
